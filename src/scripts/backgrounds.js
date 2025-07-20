fa28d44c067e5b9c00a184b199b88d9c6df530aa
Alvaro Segovia <alvarosego01@gmail.com>
2025-07-20 10:53:17 -0400

    Update video background functionality and enhance responsive styles

--- src/scripts/backgrounds.js
+++ src/scripts/backgrounds.js
@@ -1,16 +1,28 @@
 
 
+function backgroundHome() {
+
+   const videoHome = document.createElement('video');
+    videoHome.src = `${window.location.origin}/wp-content/uploads/2025/07/rostro-flotante-izquierda.mp4`;
+    videoHome.autoplay = true;
+    videoHome.loop = true;
+    videoHome.muted = true;
+    videoHome.playsInline = true;
+    videoHome.classList.add('background-hero-video');
+
+  const desktop = document.querySelector('.header-hero.home');
+  if (desktop) {
+    desktop.appendChild(videoHome);
+  }
+  const mobile = document.querySelector('.header-hero-mobile.home .header-hero-content-mobile');
+  if (mobile) {
+    mobile.appendChild(videoHome);
+  }
+
+}
+
 jQuery(document).ready(function () {
 
-  document.querySelectorAll('.header-hero.home').forEach(el => {
-    const video = document.createElement('video');
-    video.src = `${window.location.origin}/wp-content/uploads/2025/07/rostro-flotante-izquierda.mp4`;
-    video.autoplay = true;
-    video.loop = true;
-    video.muted = true;
-    video.playsInline = true;
-    video.classList.add('background-hero-video');
-    el.appendChild(video);
-  });
+  backgroundHome();
 
 });
