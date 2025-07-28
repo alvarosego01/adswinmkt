4a4cfdaf16213f78bae9503278107192a0da890d
Alvaro Segovia <alvarosego01@gmail.com>
2025-07-28 09:18:51 -0400

    Refactor code structure for improved readability and maintainability

--- src/scripts/backgrounds.js
+++ src/scripts/backgrounds.js
@@ -1,8 +1,13 @@
 
 
-const homeVideo = `${window.location.origin}/wp-content/uploads/2025/07/rostro-flotante-izquierda.mp4`;
-const servicesVideo = `${window.location.origin}/wp-content/uploads/2025/07/service-hero.webm`;
-const method360Video = `${window.location.origin}/wp-content/uploads/2025/07/metodo-360.mp4`;
+const origin = window.location.origin;
+
+const homeVideo = `${origin}/wp-content/uploads/2025/07/rostro-flotante-izquierda.mp4`;
+const servicesVideo = `${origin}/wp-content/uploads/2025/07/service-hero.mp4`;
+const method360Video = `${origin}/wp-content/uploads/2025/07/metodo-360.mp4`;
+const aboutUsVideo = `${origin}/wp-content/uploads/2025/07/nosotros-video.mp4`;
+const blogVideo = `${origin}/wp-content/uploads/2025/07/blog-video.mp4`;
+const contactVideo = `${origin}//wp-content/uploads/2025/07/contacto-video.mp4`;
 
 function backgroundHome() {
 
@@ -45,6 +50,39 @@ function backgroundHome() {
   if (method360Mobile) {
     videoFile.src = method360Video;
     method360Mobile.appendChild(videoFile);
+  }
+
+  const aboutUsDesktop = document.querySelector('.header-hero.about-us');
+  if (aboutUsDesktop) {
+    videoFile.src = aboutUsVideo;
+    aboutUsDesktop.appendChild(videoFile);
+  }
+  const aboutUsMobile = document.querySelector('.header-hero-mobile.about-us .header-hero-content-video-mobile');
+  if (aboutUsMobile) {
+    videoFile.src = aboutUsVideo;
+    aboutUsMobile.appendChild(videoFile);
+  }
+
+  const blogDesktop = document.querySelector('.header-hero.blog');
+  if (blogDesktop) {
+    videoFile.src = blogVideo;
+    blogDesktop.appendChild(videoFile);
+  }
+  const blogMobile = document.querySelector('.header-hero-mobile.blog .header-hero-content-video-mobile');
+  if (blogMobile) {
+    videoFile.src = blogVideo;
+    blogMobile.appendChild(videoFile);
+  }
+
+  const contactDesktop = document.querySelector('.header-hero.contact');
+  if (contactDesktop) {
+    videoFile.src = contactVideo;
+    contactDesktop.appendChild(videoFile);
+  }
+  const contactMobile = document.querySelector('.header-hero-mobile.contact .header-hero-content-video-mobile');
+  if (contactMobile) {
+    videoFile.src = contactVideo;
+    contactMobile.appendChild(videoFile);
   }
 
 }
